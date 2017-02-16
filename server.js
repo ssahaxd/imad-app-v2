2b153b74d88580a620ba07c2fba60b10b69c2718
Sandip Saha <ssahaxd@gmail.com>
2017-02-16 19:53:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,73 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articles = {
+    "article-one": {
+        title:'Article One | Sandip Saha',
+        heading:'Article One',
+        date:'Feb 16, 2017',
+        content:`
+            <p>This content is injected in the in the HTML template by java script, This content is going to be long enough so the user can read something if he or she wants to do so.</p>
+            <p>This is the first article and this article is not a html file, instade it is a java script injection</p>
+        `
+    },
+    "article-two":{
+        title:'Article Two | Sandip Saha',
+        heading:'Article Two',
+        date:'Feb 20, 2017',
+        content:`
+            <p>This article is known as article two, and this is also a java script injection</p>
+            <p>some extra text bla bla bla xoxo</p>
+        `
+    },
+    "article-three":{
+        title:'Article Three | Sandip Saha',
+        heading:'Article Three',
+        date:'Feb 25, 2017',
+        content:`
+            <p>This article is known as article three, and this is also a java script injection</p>
+            <p>some extra text bla bla bla xoxo</p>
+        `
+    }
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = 
+        `
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>${title}</title>
+                    <meta name="viewport" content = " width = device-width, initial-scale=1"> 
+                    <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+                <body>
+                    <div class="container">
+                        <div>
+                            <a href="/">Home</a>
+                        </div>
+                        <hr/>
+                        <h3>${header}</h3>
+                        <div>${date}</div>
+                        <div>    
+                            ${content}
+                        </div>    
+                    </div>
+                </body>
+            
+            </html>
+
+        `;
+    return htmlTemplate;
+}
+
+
 app.get('/article-one',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(creatTemplate(articles[article-one]));
 });
 
 app.get('/article-two',function (req, res){
